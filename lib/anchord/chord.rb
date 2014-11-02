37592f3f4e78b21f87afea83740b8e1c220cf734
Ejay Canaria <ejaypcanaria@gmail.com>
2014-11-02 21:48:56 +0800

    Minor refactoring

--- lib/anchord/chord.rb
+++ lib/anchord/chord.rb
@@ -10,16 +10,6 @@ module Anchord
       define_strings
     end
 
-    def define_strings
-      @tuning.each_with_index do |tune, position|
-        method_name = "#{tune}_string".to_sym
-        Chord.send(:define_method, method_name) do
-          @coordinates[position]
-        end
-        Chord.send(:alias_method, "play_#{tune}".to_sym,method_name)
-      end
-    end
-
     def name(val=nil)
       @name ||= val
     end
@@ -30,6 +20,18 @@ module Anchord
 
     def coordinates(val=nil)
       @coordinates ||= val
+    end
+
+    private
+
+    def define_strings
+      Anchord.tuning.each_with_index do |tune, position|
+        method_name = "#{tune}_string".to_sym
+        Chord.send(:define_method, method_name) do
+          @coordinates[position]
+        end
+        Chord.send(:alias_method, "play_#{tune}".to_sym,method_name)
+      end
     end
 	end
 end
